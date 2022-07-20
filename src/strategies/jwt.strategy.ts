2f1549a56f46eac2982eaf164791c56061a4e403
Nicolas Molina Monroy <hi@nicobytes.com>
2022-07-20 12:27:33 -0400

    :construction: work

--- src/strategies/jwt.strategy.ts
+++ src/strategies/jwt.strategy.ts
@@ -1,18 +1,15 @@
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable, Inject } from '@nestjs/common';
-import { ConfigType } from '@nestjs/config';
-import config from '@config/config';
+import { Injectable } from '@nestjs/common';
 
 import { TokenPayload } from '@models/token.model';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(@Inject(config.KEY) configService: ConfigType<typeof config>) {
+  constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.jwtSecretKey,
     });
   }
 
