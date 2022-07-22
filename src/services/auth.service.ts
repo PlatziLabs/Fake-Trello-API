5c8d1927d0cc9d2f55d58613eda503b3f62cfaaa
Nicolas Molina Monroy <hi@nicobytes.com>
2022-07-22 16:53:31 -0400

    :construction: work

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -48,7 +48,7 @@ export class AuthService {
   generateAccessToken(user: User) {
     const payload: TokenPayload = { userId: user.id };
     return this.jwtService.sign(payload, {
-      expiresIn: '5h',
+      expiresIn: '1m',
       secret: this.configService.accessSecretKey,
     });
   }
